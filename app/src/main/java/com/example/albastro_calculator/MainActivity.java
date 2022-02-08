3150f4acce6f36e479dc1814a9a47e4cce22fd0f
Core26AI <26renzi@gmail.com>
2022-02-08 16:53:01 +0800

    Added Functionality

--- app/src/main/java/com/example/albastro_calculator/MainActivity.java
+++ app/src/main/java/com/example/albastro_calculator/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.albastro_calculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,70 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void AddBT(View view){
+        EditText et1 =(EditText)findViewById(R.id.editTextNumberDecimal);
+        EditText et2 =(EditText)findViewById(R.id.editTextNumberDecimal2);
+        TextView et3 =(TextView)findViewById(R.id.textView3);
+
+
+        double n1 = Double.parseDouble(et1.getText().toString());
+        double n2 = Double.parseDouble(et2.getText().toString());
+        double result = n1+n2;
+
+        et3.setText("" + result);
+    }
+
+    public void SubBT(View v){
+        EditText et1 =(EditText)findViewById(R.id.editTextNumberDecimal);
+        EditText et2 =(EditText)findViewById(R.id.editTextNumberDecimal2);
+        TextView et3 =(TextView)findViewById(R.id.textView3);
+
+
+        double n1 = Double.parseDouble(et1.getText().toString());
+        double n2 = Double.parseDouble(et2.getText().toString());
+        double result = n1-n2;
+
+        et3.setText("" + result);
+        }
+
+    public void MultiplyBT(View v){
+        EditText et1 =(EditText)findViewById(R.id.editTextNumberDecimal);
+        EditText et2 =(EditText)findViewById(R.id.editTextNumberDecimal2);
+        TextView et3 =(TextView)findViewById(R.id.textView3);
+
+
+        double n1 = Double.parseDouble(et1.getText().toString());
+        double n2 = Double.parseDouble(et2.getText().toString());
+        double result = n1*n2;
+
+        et3.setText("" + result);
+        }
+
+    public void DivideBT(View v){
+        EditText et1 =(EditText)findViewById(R.id.editTextNumberDecimal);
+        EditText et2 =(EditText)findViewById(R.id.editTextNumberDecimal2);
+        TextView et3 =(TextView)findViewById(R.id.textView3);
+
+
+        double n1 = Double.parseDouble(et1.getText().toString());
+        double n2 = Double.parseDouble(et2.getText().toString());
+        double result = n1/n2;
+
+        et3.setText("" + result);
+        }
+
+    public void ModuloBT(View v){
+        EditText et1 =(EditText)findViewById(R.id.editTextNumberDecimal);
+        EditText et2 =(EditText)findViewById(R.id.editTextNumberDecimal2);
+        TextView et3 =(TextView)findViewById(R.id.textView3);
+
+
+        double n1 = Double.parseDouble(et1.getText().toString());
+        double n2 = Double.parseDouble(et2.getText().toString());
+        double result = n1%n2;
+
+        et3.setText("" + result);
+        }
+
 }
